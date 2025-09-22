b76f72839144a4e2b9c349da3c2efb6304be38e9
László Párkányi <laszlo.parkanyi@intranet.c3s.hu>
2025-09-22 17:50:09 +0200

    fix(nvim): replaced deprecated nvim-lspconfig

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,14 +1,12 @@
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
-
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "ts_ls", "clangd", "basedpyright", "vhdl_ls" }
+local servers = { "hyprls", "lua-language-server", "clangd", "basedpyright", "vhdl_ls" }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  vim.lsp.config(lsp, {
     on_attach = on_attach,
     capabilities = capabilities,
-  }
+  })
 end
