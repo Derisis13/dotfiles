1400211d76838183d43ad42e1a8636bff3bc1e03
László Párkányi <laszlo.parkanyi@intranet.c3s.hu>
2025-11-10 12:46:21 +0100

    fix(nvim): adapt to new lspconfig (partial)

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -4,9 +4,4 @@ local capabilities = require("nvchad.configs.lspconfig").capabilities
 -- if you just want default config for the servers then put them in a table
 local servers = { "hyprls", "lua-language-server", "clangd", "basedpyright", "vhdl_ls" }
 
-for _, lsp in ipairs(servers) do
-  vim.lsp.config(lsp, {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  })
-end
+vim.lsp.enable(servers)
